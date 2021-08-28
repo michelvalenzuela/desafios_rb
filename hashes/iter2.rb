8c5a28b85c11dfa85d1e3579c62f61fc50e6960c
michelvalenzuela <87387556+michelvalenzuela@users.noreply.github.com>
2021-08-27 22:39:44 -0400

    hashesdesafios

---
+++ hashes/iter2.rb
@@ -0,0 +1,26 @@
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+
+hash = {}
+
+ventas.each do |k,v|
+if v >= 45000 then 
+ hash[k] = k 
+end
+end
+
+hash.each do |k,v|
+puts k 
+end    
